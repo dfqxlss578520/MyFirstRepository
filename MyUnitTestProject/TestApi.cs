029b88d82a748f6234b2f257536b2ed64900ef65
o-shunsheng.luo <shunsheng.luo@djicorp.com>
2017-11-06 11:10:39 +0800

    测试提交

---
+++ MyUnitTestProject/TestApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyUnitTestProject
+{
+    [TestClass]
+    public class TestApi
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
